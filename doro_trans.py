8eed3d76439e7cdcf671eea8d98b12bf2d491011
smilemeeso <m950827@naver.com>
2022-01-20 11:29:15 +0900

    first commit

---
+++ doro_trans.py
@@ -0,0 +1,25 @@
+import requests
+import psycopg2
+
+# 도로명 주소 변환기
+def main():
+    conn = psycopg2.connect("dbname=localhost user=postgres password=skansmfqh@515 db=bread_mapper_dev")
+    keywords = getKeywords(conn)
+    print(keywords)
+    doSearch(keywords)
+
+def getKeywords(conn):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM store where sido_name IS NULL")
+    result = [r[0] for r in cur.fetchall()]
+
+    return result
+
+def doSearch(keywords):
+    for keyword in keywords: 
+        url = "https://www.juso.go.kr/addrlink/addrLinkApi.do?confmKey=U01TX0FVVEgyMDIyMDEyMDExMjAyMTExMjE1NzA=&currentPage=1&countPerPage=999&keyword=" + keyword
+
+        requests.get(url)
+
+        print(url)
+
